2d5839e807c3fde39dd1a1cec591c41fb02092f2
Christian Dupuis <cd@atomist.com>
2019-08-30 14:26:04 +0200

    Disable cluster for this test

--- index.ts
+++ index.ts
@@ -159,6 +159,10 @@ export const configuration: Configuration = configure(async sdm => {
                 gracePeriod: 1000 * 60 * 10,
             };
 
+            cfg.cluster = {
+                enabled: false,
+            }
+
             const isStaging = cfg.endpoints.api.includes("staging");
             if (!isStaging) {
                 cfg.listeners = [
