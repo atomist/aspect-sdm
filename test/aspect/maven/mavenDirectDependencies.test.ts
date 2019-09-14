409129eb8c5efe1d0e52df1e711f62ad5a34b9cb
Rod Johnson <rod@atomist.com>
2019-09-14 15:51:50 -0700

    Fix group/artifact ordering issue in apply

--- test/aspect/maven/mavenDirectDependencies.test.ts
+++ test/aspect/maven/mavenDirectDependencies.test.ts
@@ -119,6 +119,42 @@ describe("mavenDirectDependencies", () => {
             },
         );
 
+        it("should correctly update dependency with version to new version with artifact before group", async () => {
+                const pom = `<?xml version="1.0" encoding="UTF-8"?>
+<project>
+   <modelVersion>4.0.0</modelVersion>
+   <groupId>atomist</groupId>
+   <artifactId>cd41</artifactId>
+   <version>0.1.0-SNAPSHOT</version>
+   <packaging>jar</packaging>
+   <name>cd41</name>
+   <dependencies>
+      <dependency>
+         <artifactId>spring-boot-agent</artifactId>
+         <groupId>com.atomist</groupId>
+         <version>[2.0.0,3.0.0)</version>
+      </dependency>
+   </dependencies>
+</project>`;
+
+                const p = InMemoryProject.of(new InMemoryFile("pom.xml", pom));
+                const np = await MavenDirectDependencies.apply(p, {
+                    parameters: {
+                        fp: {
+                            data: {
+                                group: "com.atomist",
+                                artifact: "spring-boot-agent",
+                                version: "1.0.0",
+                            },
+                        },
+                    },
+                } as any) as Project;
+
+                const pf = await np.getFile("pom.xml");
+                assert(pf.getContentSync().includes("<version>1.0.0</version>"));
+            },
+        );
+
         it("should correctly update dependency with version to managed", async () => {
             const pom = `<?xml version="1.0" encoding="UTF-8"?>
 <project>
@@ -400,6 +436,8 @@ describe("mavenDirectDependencies", () => {
 </project>`;
             assert.deepStrictEqual(pf.getContentSync(), epom);
         });
+
+        it("should add dependency if it does not exist");
 
     });
 
