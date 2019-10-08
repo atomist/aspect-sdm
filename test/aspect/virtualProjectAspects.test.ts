6460802e0a94634244cd8840e06c0b137d8c567d
Christian Dupuis <cd@atomist.com>
2019-10-08 21:26:43 +0200

    Update aspect pack

---
+++ test/aspect/virtualProjectAspects.test.ts
@@ -0,0 +1,17 @@
+import { GitCommandGitProject } from "@atomist/automation-client";
+import * as assert from "assert";
+import { VirtualProjectAspects } from "../../lib/aspect/aspects";
+
+describe("VirtualProjectAspects", () => {
+
+    it("should not fail", async () => {
+        const p = await GitCommandGitProject.fromExistingDirectory({
+            owner: "sdm-org",
+            repo: "engine-and-editor",
+            branch: "master",
+        } as any, "/Users/cdupuis/Desktop/engine-and-editor");
+        const fps = await VirtualProjectAspects.extract(p, {} as any);
+        assert.strictEqual(fps, undefined);
+    });
+
+});
